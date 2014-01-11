d9f52445d374009f80bfc974d699553e737cff7f
Moudy <moudy.elkammash@gmail.com>
2014-01-11 11:28:14 -0500

    initial commit

---
+++ config/middleware.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+
+exports.configure = function (app) {
+
+  if ('development' === app.get('env')) {
+    app.use(require('./middleware/sass'));
+    app.use('/app.js', require('./middleware/browserify'));
+  }
+  app.use(express.favicon(path.join(__dirname, '..', 'public/favicon.ico')));
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+
+  app.use(function (req, res, next) {
+    res.locals.ENV = {};
+    res.locals.ENV[app.get('env')] = true;
+    next();
+  });
+
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+  app.use(app.router);
+
+  if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+    //var mongoose = require('mongoose');
+    //mongoose.set('debug', true);
+  }
+
+};
---
+++ config/settings.js
@@ -0,0 +1,19 @@
+var path = require('path');
+//var mongoose = require('mongoose');
+var view = require('./view');
+
+exports.configure = function (app) {
+  app.set('port', process.env.PORT || 3012);
+  app.set('title', 'check');
+  app.set('views', path.join(__dirname, '..', 'app', 'views'));
+  app.engine('.hbs', view.engine);
+  app.set('view engine', '.hbs');
+
+  if ('development' === app.get('env')) {
+    require('node-pow')(app);
+    app.set('host', app.get('title')+'.dev');
+  } else if ('production' === app.get('env')) {
+    app.set('host', app.get('title')+'.com');
+  }
+};
+
