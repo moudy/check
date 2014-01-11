3abb05c240883d79c38d107574bb5ef9cfab731e
Moudy <moudy.elkammash@gmail.com>
2014-01-11 12:50:39 -0500

    ember setup

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,1 +1,14 @@
-require('./hello');
+/* globals document: false */
+var FastClick = require('fastClick');
+require('jquery');
+global.Handlebars = require('handlebars');
+require('./vendor/ember');
+require('./templates');
+
+var App = exports.instance = global.App = Em.Application.create();
+
+if (global.history && global.history.pushState) {
+  App.Router.reopen({ location: 'history' });
+}
+
+Em.$(function() { FastClick.attach(document.body); });
