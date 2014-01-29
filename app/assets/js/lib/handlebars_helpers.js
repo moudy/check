2a0d1fe1c14237f47e57634fa4208755a4cb5806
Moudy <moudy.elkammash@gmail.com>
2014-01-29 10:41:49 -0500

    default to empty string

--- app/assets/js/lib/handlebars_helpers.js
+++ app/assets/js/lib/handlebars_helpers.js
@@ -2,6 +2,7 @@ var markdown = require('./markdown');
 
 exports.register = function (Handlebars) {
   Handlebars.helper('markdown', function(value) {
+    if (!value) return '';
     return new Handlebars.SafeString(markdown(value));
   });
 };
