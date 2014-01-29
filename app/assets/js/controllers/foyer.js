339dd70845b86500b22c1f7f400060cc16c62b95
Moudy <moudy.elkammash@gmail.com>
2014-01-29 10:55:20 -0500

    fix foyer

--- app/assets/js/controllers/foyer.js
+++ app/assets/js/controllers/foyer.js
@@ -1,11 +1,11 @@
 var App = require('../app').instance;
 
 var listItems = [
-  {title:'Read the Checklist Manifesto', isCompleted:true}
-, {title:'Create an Account', isActive:true}
-, {title:'Make your first list'}
-, {title:'Share your list with others'}
-, {title:'Adapt someone else\'s list'}
+  {description: '## Read the Checklist Manifesto', isCompleted:true}
+, {description: '## Create an Account', isActive:true}
+, {description: '## Make your first list'}
+, {description: '## Share your list with others'}
+, {description: '## Adapt someone else\'s list'}
 ];
 
 App.FoyerController = Em.Controller.extend({
