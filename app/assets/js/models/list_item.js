0a76bac1a07a650e58c49e2b270defe893a64c27
Moudy <moudy.elkammash@gmail.com>
2014-03-04 11:46:40 -0500

    save completed state in localstorage

--- app/assets/js/models/list_item.js
+++ app/assets/js/models/list_item.js
@@ -6,5 +6,15 @@ App.ListItem = DS.Model.extend({
 , checklistId: attr('string')
 , checklist: DS.belongsTo('checklist', {async:true})
 , index: attr('number')
+, isCompleted: function (key, value) {
+    var lsKey = 'ListItem:'+this.get('id')+':isCompleted';
+    if (window.localStorage) {
+      if (arguments.length > 1) window.localStorage.setItem(lsKey, value);
+      return window.localStorage.getItem(lsKey) === 'true';
+    } else {
+      if (arguments.length > 1) this.set(key, value);
+      return this.get(key) === 'true';
+    }
+  }.property()
 });
 
