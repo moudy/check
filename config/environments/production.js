5bf8e963327e5a1252f2651429cf8aae05be76e2
Moudy <moudy.elkammash@gmail.com>
2014-03-08 18:21:24 -0500

    fix production env vars

--- config/environments/production.js
+++ config/environments/production.js
@@ -1,10 +1,8 @@
-var env = require('../env').production;
-
 exports.configure = function (app) {
   app.set('port', process.env.PORT);
   app.set('MONGO_URI', process.env.MONGOLAB_URI);
   app.set('host', 'chck.herokuapp.com');
-  app.set('GITHUB_CLIENT_ID', env.GITHUB_CLIENT_ID);
-  app.set('GITHUB_CLIENT_SECRET', env.GITHUB_CLIENT_SECRET);
+  app.set('GITHUB_CLIENT_ID', process.env.GITHUB_CLIENT_ID);
+  app.set('GITHUB_CLIENT_SECRET', process.env.GITHUB_CLIENT_SECRET);
 };
 
